176bb144292746a3cad727e1c4d6a637c90e4173
Herbert Valerio Riedel <hvr@gnu.org>
2017-08-13 10:16:45 +0200

    Update instructions for building the PVP's site HTML
    This should help address #17

--- pvp-site.sh
+++ pvp-site.sh
@@ -1,8 +1,23 @@
 #!/bin/bash -e
 
 shopt -s globstar
-set -x
 
-cabal new-build exe:pvp-site
+case $(cabal --numeric-version) in
+    2.0.*)
+        echo "INFO: Detected cabal 2.0; using 'new-run' emulation - if this fails, try removing dist-newstyle/ or upgrade to cabal 2.1+"
 
-dist-newstyle/build/**/pvp-site-1.0/**/build/pvp-site/pvp-site "$@"
+        cabal new-build exe:pvp-site
+
+        set -x
+        exec dist-newstyle/build/**/pvp-site-1.0/**/build/pvp-site/pvp-site "$@"
+        ;;
+
+    2.*)
+        echo "INFO: Detected cabal 2.1+"
+        exec cabal new-run exe:pvp-site -- "$@"
+        ;;
+    *)  echo "ERROR: Sorry, you need at least cabal 2.0"
+        exit 1
+        ;;
+
+esac
