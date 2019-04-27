02aec57b6a645882991fc105298093956ef8ddb3
Herbert Valerio Riedel <hvr@gnu.org>
2019-04-27 16:37:33 +0200

    Update pvp-site.sh script

--- pvp-site.sh
+++ pvp-site.sh
@@ -3,8 +3,13 @@
 shopt -s globstar
 
 case $(cabal --numeric-version) in
-    2.0.*)
-        echo "INFO: Detected cabal 2.0; using 'new-run' emulation - if this fails, try removing dist-newstyle/ or upgrade to cabal 2.1+"
+    1.*)
+        echo "ERROR: Sorry, you need at least cabal 2.0"
+        exit 1
+        ;;
+
+    2.[01].*)
+        echo "INFO: Detected cabal 2.0; using 'new-run' emulation - if this fails, try removing dist-newstyle/ or upgrade to cabal 2.2 or later"
 
         cabal new-build exe:pvp-site
 
@@ -12,11 +17,17 @@ case $(cabal --numeric-version) in
         exec dist-newstyle/build/**/pvp-site-1.0/**/build/pvp-site/pvp-site "$@"
         ;;
 
-    2.*)
-        echo "INFO: Detected cabal 2.1+"
+    2.[23].*)
+        echo "INFO: Detected cabal 2.2"
         exec cabal new-run exe:pvp-site -- "$@"
         ;;
-    *)  echo "ERROR: Sorry, you need at least cabal 2.0"
+
+    2.[45].*|3.*)
+        echo "INFO: Detected cabal 2.4+"
+        exec cabal v2-run exe:pvp-site -- "$@"
+        ;;
+
+    *)  echo "ERROR: Unknown version of cabal detected; you need at least cabal 2.0"
         exit 1
         ;;
 
